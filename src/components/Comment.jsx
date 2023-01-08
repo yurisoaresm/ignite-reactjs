fd5e73f60a35cab5c71470c15abb4f5b015560f9
Yuri Soares <yurisoaresm@outlook.com>
2023-01-08 17:38:47 -0300

    feat: create comments component structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/yurisoaresm.png" />
+      
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Yuri Soares</strong>
+
+              <time title="03 de Janeiro às 17:59h" dateTime="2023-01-03 17:59:23">Cerca de 1h atrás</time>
+
+              <button title='Deletar comentário'>
+                <Trash />
+              </button>
+
+            </div>
+          </header>
+
+          <p>Conteúdo do comentário.</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
